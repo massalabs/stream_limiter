06b867db3bd92e8e62e3a68f4512168c54a62186
Litchi Pi <litchi.pi@proton.me>
2023-05-15 11:00:12 +0200

    Fixup doctest
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! use std::fs::File;
 //!
 //! let mut file = File::open("tests/resources/test.txt").unwrap();
-//! let mut limiter = Limiter::new(file, 1, Duration::from_secs(1));
+//! let mut limiter = Limiter::new(file, 1, Duration::from_secs(1), 1);
 //! let mut buf = [0u8; 10];
 //! let now = std::time::Instant::now();
 //! limiter.read(&mut buf).unwrap();
